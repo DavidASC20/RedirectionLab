850775ceba776e5de838bdef8f10e58869979c8c
David Chong <djchong24@gmail.com>
2021-01-05 15:27:08 -0500

    final commit, works

--- MakeStars.java
+++ MakeStars.java
@@ -1,18 +1,23 @@
 import java.util.Scanner;
+
+import javax.swing.text.Style;
 public class MakeStars {
 
     public static void main(String[] args) {
         String newLine = "";
-        String temp = "";
         Scanner input = new Scanner(System.in);
         while(input.hasNextLine()){
             Scanner lineRead = new Scanner(input.nextLine());
             while(lineRead.hasNext()){
+                String temp = "";
+                // System.out.println(lineRead.next());
                 int x = lineRead.next().length();
-                for(int i = 0; i < x - 1; i++){
+                for(int i = 0; i < x; i++){
                     temp += "*";
                 }
+                // System.out.println(temp);
                 newLine += temp + " ";
+
             }System.out.println(newLine);
                 newLine = "";
         }
