bbae1f474bc34688728459b42a29d2a9e4e5f5eb
David Chong <djchong24@gmail.com>
2021-01-05 14:51:41 -0500

    commit

--- MakeStars.java
+++ MakeStars.java
@@ -6,14 +6,16 @@ public class MakeStars {
         String temp = "";
         Scanner input = new Scanner(System.in);
         while(input.hasNextLine()){
-            Scanner lineRead = new Scanner(System.in);
+            Scanner lineRead = new Scanner(input.nextLine());
             while(lineRead.hasNext()){
                 int x = lineRead.next().length();
                 for(int i = 0; i < x - 1; i++){
                     temp += "*";
                 }
                 newLine += temp + " ";
+                System.out.println(newLine);
+                newLine = "";
             }
-        }System.out.print(newLine);
+        }
     }
 }
